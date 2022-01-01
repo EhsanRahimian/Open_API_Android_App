5b9a49ca47555ad80235a9e52b53e404480a7819
Ethan Rahimian <nicootech@gmail.com>
2022-01-01 11:06:07 -0800

    Setting up Room Persistence

--- app/src/main/java/com/nicootech/open_api_android_app/persistence/AccountPropertiesDao.kt
+++ app/src/main/java/com/nicootech/open_api_android_app/persistence/AccountPropertiesDao.kt
@@ -1,3 +1,23 @@
 package com.nicootech.open_api_android_app.persistence
 
-interface AccountPropertiesDao
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.nicootech.open_api_android_app.models.AccountProperties
+
+@Dao
+interface AccountPropertiesDao{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAndReplace(account_properties: AccountProperties): Long
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertOrIgnore(account_properties: AccountProperties): Long
+
+    @Query("SELECT * FROM account_properties WHERE pk = :pk")
+    fun searchByPk(pk: Int): LiveData<AccountProperties>
+
+    @Query("SELECT * FROM account_properties WHERE email = :email")
+    fun searchByEmail(email: String): AccountProperties?
+}
--- app/src/main/java/com/nicootech/open_api_android_app/persistence/AppDatabase.kt
+++ app/src/main/java/com/nicootech/open_api_android_app/persistence/AppDatabase.kt
@@ -1,5 +1,19 @@
 package com.nicootech.open_api_android_app.persistence
 
-abstract class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.nicootech.open_api_android_app.models.AccountProperties
+import com.nicootech.open_api_android_app.models.AuthToken
+
+@Database(entities = [AuthToken::class, AccountProperties::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+
+    abstract fun getAuthTokenDao(): AuthTokenDao
+
+    abstract fun getAccountPropertiesDao(): AccountPropertiesDao
+
+    companion object{
+        const val DATABASE_NAME: String = "app_db"
+    }
 
 }
